53be213a49913e017c33143d6dc30ed50c364858
Ancentus <mancentus7@gmail.com>
2024-02-24 08:49:07 +0300

    Edit paths | remove auth

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import CategoryList, ProductList
 
 urlpatterns = [
-    path('categories/', CategoryList.as_view(), name='category-list'),
-    path('products/', ProductList.as_view(), name='product-list'),
+    path('api/categories/', CategoryList.as_view(), name='category-list'),
+    path('api/products/', ProductList.as_view(), name='product-list'),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -5,7 +5,9 @@ from .serializers import CategorySerializer, ProductSerializer
 class CategoryList(generics.ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = []
 
 class ProductList(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = []
