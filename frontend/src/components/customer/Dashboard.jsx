eb59e71686bb4bbad32cfc230eb3f5b5183a445b
Ancentus <mancentus7@gmail.com>
2024-02-24 22:28:41 +0300

    Create dashboard component

---
+++ frontend/src/components/customer/Dashboard.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+import Orders from './Orders';
+
+class Dashboard extends React.Component {
+  render() {
+    return (
+      <div id="content">
+        <div className="container">
+          <div className="col-md-12">
+            <ul className="breadcrumb">
+              <li>
+                <a href="../index.php">Home</a>
+              </li>
+              <li>
+                My Orders
+              </li>
+            </ul>
+          </div>
+          <div className="col-md-3">
+            <Sidebar />
+          </div>
+          <div className="col-md-9">
+            <div className="box">
+              <Orders />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
