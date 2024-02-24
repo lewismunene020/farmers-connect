269358f647c7ae8ea63475f3b5858f77839b5dbe
Ancentus <mancentus7@gmail.com>
2024-02-24 08:27:10 +0300

    Define views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
 
-# Create your views here.
+class CategoryList(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class ProductList(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
