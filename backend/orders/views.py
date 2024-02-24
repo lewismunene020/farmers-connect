247d39e4676d2310a29b5ae19b9274876a0fa193
Ancentus <mancentus7@gmail.com>
2024-02-25 02:15:53 +0300

    Define order views

--- backend/orders/views.py
+++ backend/orders/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .models import Order
+from .serializers import OrderSerializer
 
-# Create your views here.
+class OrderDetailAPIView(generics.RetrieveAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+class OrderCreateAPIView(generics.CreateAPIView):
+    serializer_class = OrderSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(customer=self.request.user)
+
+class CustomerOrdersAPIView(generics.ListCreateAPIView):
+    serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        customer_id = self.kwargs.get('customer_id')
+        return Order.objects.filter(customer_id=customer_id)
+
+class FarmerOrdersAPIView(generics.ListAPIView):
+    serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        farmer_id = self.kwargs.get('farmer_id')
+        return Order.objects.filter(farmer_id=farmer_id)
+
+class OrderUpdateAPIView(generics.UpdateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+class OrderDeleteAPIView(generics.DestroyAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
