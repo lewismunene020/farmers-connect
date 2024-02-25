7b9ac64c5cbb44e8e22287dfe4098e5209a4d114
Ancentus <mancentus7@gmail.com>
2024-02-25 18:33:31 +0300

    Define views

--- backend/farms/views.py
+++ backend/farms/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Farm
+from .serializers import FarmSerializer
 
-# Create your views here.
+class FarmDetailAPIView(generics.RetrieveAPIView):
+    queryset = Farm.objects.all()
+    serializer_class = FarmSerializer
+
+class FarmerFarmsAPIView(generics.ListAPIView):
+    serializer_class = FarmSerializer
+
+    def get_queryset(self):
+        farmer_id = self.kwargs.get('farmer_id')
+        return Farm.objects.filter(farmer_id=farmer_id)
+
+class FarmUpdateAPIView(generics.UpdateAPIView):
+    queryset = Farm.objects.all()
+    serializer_class = FarmSerializer
+
+class FarmDeleteAPIView(generics.DestroyAPIView):
+    queryset = Farm.objects.all()
+    serializer_class = FarmSerializer
+
+class FarmListAPIView(generics.ListAPIView):
+    queryset = Farm.objects.all()
+    serializer_class = FarmSerializer
