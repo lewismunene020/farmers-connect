d7100272121e016b3fd6724e64453f05fbab1a9f
Ancentus <mancentus7@gmail.com>
2024-02-25 18:30:50 +0300

    Define model

--- backend/farms/models.py
+++ backend/farms/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from api.models import Product, County, SubCounty
 
-# Create your models here.
+class Farm(models.Model):
+    farm_id = models.AutoField(primary_key=True)
+    farmer = models.ForeignKey(User, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    product_image1 = models.ImageField(upload_to='images/')
+    product_image2 = models.ImageField(upload_to='images/')
+    location_county_id = models.ForeignKey(County, on_delete=models.CASCADE)
+    location_subcounty_id = models.ForeignKey(SubCounty, on_delete=models.CASCADE)
+    quantity_available = models.PositiveIntegerField()
+    price_per_unit = models.DecimalField(max_digits=10, decimal_places=2)
