b1f768bee1aa09038ad42e43295cf9b5db906c2e
Ancentus <mancentus7@gmail.com>
2024-02-25 20:16:09 +0300

    Setup media URL

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -19,6 +19,9 @@ import  dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,4 +25,4 @@ urlpatterns = [
     path('', include("api.urls")),
     path('', include("orders.urls")),
     path('', include("farms.urls"))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
