9cb1e97ca952a2aaebc21dbe07b94f8364f19aec
lewismunene020 <lewismunene020@gmail.com>
2024-02-27 09:14:46 +0300

    added the django feature

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from  .models import County ,SubCounty ,Product ,Category
 # Register your models here.
+
+admin.site.register(County)
+admin.site.register(SubCounty)
+admin.site.register(Product)
+admin.site.register(Category)
+
