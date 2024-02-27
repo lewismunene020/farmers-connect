bbf62f25402fc758c35ba0d37ef49cbf748c4170
Ancentus <mancentus7@gmail.com>
2024-02-28 02:26:36 +0300

    Create public FarmService
    Co-authored-by: Shem Opiyo <shemnicholasopiyo@gmail.com>

---
+++ frontend/src/services/PublicFarmService.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:8000/api/';
+
+const api = axios.create({
+    baseURL: BASE_URL,
+});
+
+const PublicFarmService = {
+    getCategories: () => api.get('categories/'),
+    getProductsByCategory: (categoryId) => api.get(`products/category/${categoryId}`),
+    getCounties: () => api.get('counties/'),
+    getSubCountiesByCounty: (countyId) => api.get(`subcounties/county/${countyId}`),
+    getFarmsByFarmerId: (farmerId) => api.get(`farms/farmer/${farmerId}/`),
+};
+
+export default PublicFarmService;
