c8891ce2bc3a271b826f24b4ee08114047a666c3
Ancentus <mancentus7@gmail.com>
2024-03-03 20:40:58 +0300

    Filter orders

--- backend/orders/serializers.py
+++ backend/orders/serializers.py
@@ -1,11 +1,21 @@
 from rest_framework import serializers
 from .models import Order
-from api.models import Product
+from api.models import Product, County, SubCounty
 from accounts.models import User
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
+        fields = '__all__'
+
+class CountySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = County
+        fields = '__all__'
+
+class SubCountySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubCounty
         fields = '__all__'
 
 class FarmerSerializer(serializers.ModelSerializer):
@@ -21,6 +31,8 @@ class CustomerSerializer(serializers.ModelSerializer):
 class OrderSerializer(serializers.ModelSerializer):
     customer = CustomerSerializer()
     product_id = ProductSerializer()
+    county_id = CountySerializer()
+    subcounty_id = SubCountySerializer()
     farmer = FarmerSerializer()
 
     class Meta:
