0d205e8feaa73d1c6d8ef45ee2d4a5713d7e7109
shem-opiyo <shemnicholasopiyo@gmail.com>
2024-03-07 21:36:51 +0300

    modified the User class in models.py in accounts

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -9,7 +9,6 @@ from rest_framework.request import Request
 def  generate_unique_id():
     return uuid4().hex
 
-# Create your models here.
 class UserManager(BaseUserManager):
     def create_user(self, email, password, **extra_fields):
         if not email:
